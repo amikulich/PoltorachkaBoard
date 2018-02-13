dbcc390dcf77b69146b3d5269503c25c7fd33234
Artem Mikulich <artemmikulich@gmail.com>
2018-02-13 08:55:53 +0300

    Data scripts

---
+++ Poltorachka/DbScripts/003_insert_individuals.sql
@@ -0,0 +1,16 @@
+﻿IF NOT EXISTS (SELECT 1 FROM individual)
+BEGIN
+	INSERT INTO [dbo].[individual]
+			   ([name])
+		 VALUES
+		        ('Artem Mikulich')
+			   ,('Andrey Ravkov')
+			   ,('Sergey Morgachev')
+			   ,('Alexey Rozdyalovsky')
+			   ,('Rita Khodarkevich')
+			   ,('Irina Makarevich')
+			   ,('Pavel Yuruts')
+			   ,('Shamil Salsanov')
+			   ,('Dmitry Stashevsky')
+			   ,('Stan Altukhov')
+END
---
+++ Poltorachka/DbScripts/004_connect_individual_and_user.sql
@@ -0,0 +1,10 @@
+﻿DECLARE @userId UNIQUEIDENTIFIER = 'd3e24dfd-4f27-4ac1-9009-39c86b03240d',
+@userName NVARCHAR(255) = 'Artem Mikulich';
+
+
+IF EXISTS (SELECT 1 FROM individual WHERE name = @userName AND [user_id] IS NULL)
+BEGIN
+	UPDATE individual 
+	SET [user_id] = @userId
+	WHERE name = @userName
+END;
