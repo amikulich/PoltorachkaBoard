4ae7f6caa8d3dd7951f57f7f422f4f981fb0cadc
Artem Mikulich <artemmikulich@gmail.com>
2018-02-17 23:55:03 +0300

    Simplify authentication

--- Poltorachka/Startup.cs
+++ Poltorachka/Startup.cs
@@ -5,10 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Poltorachka.Data;
 using Poltorachka.DataAccess;
 using Poltorachka.Domain;
+using Poltorachka.Models;
 
 namespace Poltorachka
 {
@@ -24,38 +28,47 @@ namespace Poltorachka
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MainDb")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
             services.AddSingleton<IConfiguration>(Configuration);
             services.AddSingleton<IFactRepository, FactRepository>();
             services.AddSingleton<IFactSummaryQuery, FactSummaryQuery>(); 
             services.AddSingleton<IIndividualsQuery, IndividualsQuery>();
-            services.AddMvc();
 
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
-            var authSection = Configuration.GetSection("Authentication");
+            //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
-            services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = "Cookies";
-                    options.DefaultChallengeScheme = "oidc";
-                })
-                .AddCookie("Cookies")
-                .AddOpenIdConnect("oidc", options =>
-                {
-                    options.SignInScheme = "Cookies";
+            //var authSection = Configuration.GetSection("Authentication");
 
-                    options.Authority = authSection.GetValue<string>("Url");
-                    options.RequireHttpsMetadata = false;
+            //services.AddAuthentication(options =>
+            //    {
+            //        options.DefaultScheme = "Cookies";
+            //        options.DefaultChallengeScheme = "oidc";
+            //    })
+            //    .AddCookie("Cookies")
+            //    .AddOpenIdConnect("oidc", options =>
+            //    {
+            //        options.SignInScheme = "Cookies";
 
-                    options.ClientId = authSection.GetValue<string>("ClientId");
-                    options.ClientSecret = authSection.GetValue<string>("ClientSecret");
-                    options.ResponseType = "code id_token";
+            //        options.Authority = authSection.GetValue<string>("Url");
+            //        options.RequireHttpsMetadata = false;
 
-                    options.SaveTokens = true;
-                    options.GetClaimsFromUserInfoEndpoint = true;
+            //        options.ClientId = authSection.GetValue<string>("ClientId");
+            //        options.ClientSecret = authSection.GetValue<string>("ClientSecret");
+            //        options.ResponseType = "code id_token";
 
-                    options.Scope.Add("Poltorachka.Identity");
-                });
+            //        options.SaveTokens = true;
+            //        options.GetClaimsFromUserInfoEndpoint = true;
+
+            //        options.Scope.Add("Poltorachka.Identity");
+            //    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
